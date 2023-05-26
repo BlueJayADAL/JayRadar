d69be858fe484b6236cef54c1eebee68e13f9242
Nathaniel Brightup <brightupn@etown.edu>
2023-05-26 10:25:18 -0400

    Version 4 with customized spinbox

---
+++ GUI/main_gui.py
@@ -0,0 +1,113 @@
+import tkinter as tk
+from tkinter import Frame, OptionMenu
+import cv2
+from PIL import ImageTk, Image
+from spinbox import Spinbox
+
+def filter_option_selected(selected_option):
+    """Callback function when the filter dropdown is changed"""
+    print("Filter Mode: ", selected_option)
+
+def model_option_selected(selected_option):
+    """Callback function when the model dropdown is changed"""
+    print("Model: ", selected_option)
+
+def resize(event):
+    """Adjust the size of the frames to fit the window"""
+    mainwin.grid_columnconfigure(0, weight=1)
+    mainwin.grid_columnconfigure(1, weight=1)
+    mainwin.grid_columnconfigure(2, weight=1)
+    mainwin.grid_rowconfigure(0, weight=1)
+
+def show_frames():
+    """Retrieve frames from the camera and update the GUI"""
+    ret, frame = cap.read()  # Read frame from the camera
+    if ret:
+        frame = cv2.flip(frame, 1)  # Flip the frame horizontally
+
+        # Convert the color channels from BGR to RGB
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Create a PIL ImageTk object
+        img = ImageTk.PhotoImage(image=Image.fromarray(frame_rgb))
+
+        cam.imgtk = img  # Keep a reference to the image to prevent garbage collection
+        cam.configure(image=img)  # Update the Label widget with the new image
+
+    cam.after(10, show_frames)  # Schedule the next frame update after 30 milliseconds
+
+# Create the main window
+mainwin = tk.Tk()
+mainwin.title("Overall GUI")
+mainwin.geometry("889x500")
+mainwin.resizable(True, True)
+mainwin.minsize(width=889, height=500)
+mainwin.bind("<Configure>", resize)
+
+# Create the internal frames
+frame_1 = Frame(mainwin, bg='grey')
+frame_1.grid(row=0, column=0, sticky="nsew", padx=1)
+
+frame_2 = Frame(mainwin, bg='grey')
+frame_2.grid(row=0, column=1, sticky="nsew", padx=1)
+
+camframe = Frame(mainwin, bg='grey')
+camframe.grid(row=0, column=2, sticky="nsew", padx=1)
+
+# Configure the layout of the main window
+mainwin.grid_columnconfigure(0, weight=1, minsize=180)
+mainwin.grid_columnconfigure(1, weight=1, minsize=160)
+mainwin.grid_columnconfigure(2, weight=2)
+mainwin.grid_rowconfigure(0, weight=1, minsize=500)
+
+# Create the widgets in frame_1 ('Tuning')
+Tuning = tk.Label(frame_1, bg='grey', text="Tuning", font=("Arial Bold", 30)).grid(row=0, column=0)
+
+Conf_Thresh = tk.Label(frame_1, bg='grey', text="Confidence Threshold:", font=("Arial Bold", 12)).grid(row=1, column=0)
+Conf_Thresh_Spin = Spinbox(frame_1, min=0, max=100, increment=1, default_value=50).grid(row=2, column=0)
+
+NMS_Thresh = tk.Label(frame_1, bg='grey', text="NMS Threshold:", font=("Arial Bold", 12)).grid(row=3, column=0)
+NMS_Thresh_Spin = Spinbox(frame_1, min=0, max=100, increment=1, default_value=50).grid(row=4, column=0)
+
+filter_type = tk.Label(frame_1, bg='grey', text="Filter mode: ", font=("Arial Bold", 12), justify="center").grid(row=5, column=0)
+selected_filter_option = tk.StringVar(mainwin)
+selected_filter_option.set("Closest")  # Set default option
+Filter_Options = ["Closest", "Highest Confidence"]
+Filter = OptionMenu(frame_1, selected_filter_option, *Filter_Options, command=filter_option_selected).grid(row=6, column=0)
+
+# Create the widgets in frame_2 ('Model')
+Model_Label = tk.Label(frame_2, bg='grey', text="Model", font=("Arial Bold", 40), justify="center").grid(row=0, column=0)
+
+model_type = tk.Label(frame_2, bg='grey', text="Model: ", font=("Arial Bold", 12), justify="center").grid(row=1, column=0)
+
+selected_model_option = tk.StringVar(mainwin)
+selected_model_option.set("1")  # Set default option
+Model_Options = ["1", "2", "3", "4"]
+Model_OptionMenu = OptionMenu(frame_2, selected_model_option, *Model_Options, command=model_option_selected)
+Model_OptionMenu.grid(row=2, column=0)
+
+res_label = tk.Label(frame_2, bg='grey', text="Resolution", font=("Arial Bold", 18), justify="center").grid(row=3, column=0)
+res_frame = Frame(frame_2, bg='grey')
+res_frame.grid(row=4, column=0, sticky="nsew")
+
+res_width = tk.Label(res_frame, bg='grey', text="Width", font=("Arial Bold", 12), justify="center").grid(row=0, column=0)
+res_width_spin = Spinbox(res_frame, min=100, max=9999, increment=1, default_value=400)
+res_width_spin.grid(row=1, column=0)
+
+res_height = tk.Label(res_frame, bg='grey', text="Height", font=("Arial Bold", 12), justify="center").grid(row=2, column=0)
+res_height_spin = Spinbox(res_frame, min=100, max=9999, increment=1, default_value=400)
+res_height_spin.grid(row=3, column=0)
+
+# Create the widgets in camframe (camera output)
+cam = tk.Label(camframe)
+cam.grid(row=0, column=0)
+calc_disp = tk.Label(camframe, bg='grey', text="Output Information: (x, y, area)", font=("Arial Bold", 12), justify="center").grid(row=1, column=0)
+
+# Open the video capture
+cap = cv2.VideoCapture(0)
+
+# Start displaying frames
+show_frames()
+
+# Start the GUI event loop
+mainwin.mainloop()
---
+++ GUI/spinbox.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+
+class Spinbox(tk.Frame):
+    def __init__(self, parent, default_value=50, min=0, max=100, increment=1, **kwargs):
+        super().__init__(parent, **kwargs)
+
+        self.min = min
+        self.max = max
+        self.increment = increment
+
+        self.button_frame = tk.Frame(self)
+        self.button_frame.pack(side=tk.LEFT)
+
+        self.subtract_button = tk.Button(self.button_frame, text="-", command=self.subtract, bg='grey')
+        self.subtract_button.pack(side=tk.LEFT)
+
+        self.entry = tk.Entry(self, width=8)
+        self.entry.pack(side=tk.LEFT)
+        self.set(default_value)
+
+        self.buttonplus_frame = tk.Frame(self)
+        self.buttonplus_frame.pack(side=tk.RIGHT)
+
+        self.add_button = tk.Button(self.buttonplus_frame, text="+", command=self.add, bg='grey')
+        self.add_button.pack(side=tk.RIGHT)
+
+    def subtract(self):
+        current_value = self.get()
+        new_value = current_value - self.increment
+        if new_value >= self.min:
+            self.set(new_value)
+
+    def add(self):
+        current_value = self.get()
+        new_value = current_value + self.increment
+        if new_value <= self.max:
+            self.set(new_value)
+
+    def get(self):
+        try:
+            value = int(self.entry.get())
+            if self.min <= value <= self.max:
+                return value
+        except ValueError:
+            pass
+        return self.min
+
+    def set(self, value):
+        if self.min <= value <= self.max:
+            self.entry.delete(0, tk.END)
+            self.entry.insert(0, str(value))
