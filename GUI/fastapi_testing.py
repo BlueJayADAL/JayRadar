be0d36c9ca4fcd3ff56bcedaecfd7c9cff76b8a2
klinefelters <stevenjk03@gmail.com>
2023-06-16 13:22:57 -0400

    added fastapi testing to GUI dir

--- GUI/fastapi_testing.py
+++ GUI/fastapi_testing.py
@@ -1,8 +1,5 @@
 import cv2
 from fastapi import FastAPI, Request
-from PIL import Image
-from io import BytesIO
-from capture import frame_queue, process_event
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import StreamingResponse
 
@@ -17,23 +14,31 @@ def get_frame():
         ret, frame = cap.read()
         ret, buffer = cv2.imencode('.jpg', frame)
         frame_data = buffer.tobytes()
-            
+
         # Yield the frame data as MJPEG response
-        yield (b'--frame\r\n'
-            b'Content-Type: image/jpeg\r\n\r\n' + frame_data + b'\r\n')
+        yield (
+            b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + frame_data + b'\r\n'
+        )
+
 
 @app.get('/')
 def index(request: Request):
     # Render the index.html template with current confidence threshold value
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 @app.get('/video_feed')
 def video_feed():
     # Route for streaming video feed
-    return StreamingResponse(get_frame(), media_type='multipart/x-mixed-replace; boundary=frame')
+    return StreamingResponse(
+        get_frame(), media_type='multipart/x-mixed-replace; boundary=frame'
+    )
+
 
 if __name__ == "__main__":
     import uvicorn
-    from constants import SOCKET_IP
 
-    uvicorn.run(app, host=SOCKET_IP, port=8000)
+    ip_addr = '10.1.80.32'
+
+    uvicorn.run(app, host=ip_addr, port=8000)
