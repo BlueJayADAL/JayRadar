d533ce9db5aa2622a394d0ab8b1a72de4f8a9d52
klinefelters <stevenjk03@gmail.com>
2023-06-16 23:45:35 -0400

    Created a webpage GUI. Now to hook it up.

--- GUI/fastapi_testing.py
+++ GUI/fastapi_testing.py
@@ -1,36 +1,68 @@
 import cv2
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 def get_frame():
     cap = cv2.VideoCapture(0)
     while True:
-        # Get the latest frame from the queue
         ret, frame = cap.read()
         ret, buffer = cv2.imencode('.jpg', frame)
         frame_data = buffer.tobytes()
 
-        # Yield the frame data as MJPEG response
         yield (
             b'--frame\r\n'
             b'Content-Type: image/jpeg\r\n\r\n' + frame_data + b'\r\n'
         )
 
 
+class Slider(BaseModel):
+    key: str
+    value: float
+
+
+class Checkbox(BaseModel):
+    key: str
+    value: bool
+
+
+class TextBox(BaseModel):
+    key: str
+    value: str
+
+
 @app.get('/')
 def index(request: Request):
-    # Render the index.html template with current confidence threshold value
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post('/sliders')
+def sliders(slider: Slider):
+    print(f"Slider: {slider.key} = {slider.value}")
+    return {"message": "Slider value received"}
+
+
+@app.post('/checkboxes')
+def checkboxes(checkbox: Checkbox):
+    print(f"Checkbox: {checkbox.key} = {checkbox.value}")
+    return {"message": "Checkbox value received"}
+
+
+@app.post('/textboxes')
+def textboxes(textbox: TextBox):
+    print(f"Textbox: {textbox.key} = {textbox.value}")
+    return {"message": "Textbox value received"}
 
 
 @app.get('/video_feed')
 def video_feed():
-    # Route for streaming video feed
     return StreamingResponse(
         get_frame(), media_type='multipart/x-mixed-replace; boundary=frame'
     )
@@ -39,6 +71,6 @@ def video_feed():
 if __name__ == "__main__":
     import uvicorn
 
-    ip_addr = '10.1.80.32'
+    ip_addr = '10.1.32.27'
 
     uvicorn.run(app, host=ip_addr, port=8000)
