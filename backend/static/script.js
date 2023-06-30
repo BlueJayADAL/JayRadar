2e530c8fa3ad6c56863fddd1db898bebfef20998
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2023-06-30 00:21:52 -0400

    Rewrote script.js to consolidate socket listeners

--- backend/static/script.js
+++ backend/static/script.js
@@ -1,86 +1,71 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const debuggingForm = document.getElementById("debugging-form");
-    const keyInput = document.getElementById("key-input");
-    const valueInput = document.getElementById("value-input");
-    const sliders = document.querySelectorAll('input[type="range"]');
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    const configDropdown = document.getElementById("config");
-    const saveButton = document.getElementById("saveButton");
-    const resetButton = document.getElementById("resetButton");
-    const socket = new WebSocket(`ws://${location.host}/ws`);
-  
-    socket.onopen = () => {
-      console.log("WebSocket connection established.");
-    };
-  
-    const sendMessage = (user, message) => {
-      const formattedMessage = `${user}: ${message}`;
-      socket.send(formattedMessage);
-    };
-  
-    debuggingForm.addEventListener("submit", (event) => {
-      event.preventDefault();
-      const user = keyInput.value;
-      const message = valueInput.value;
-      sendMessage(user, message);
-      valueInput.value = "";
-    });
-  
-    sliders.forEach(slider => {
-      const sliderValue = document.getElementById(`${slider.id}Value`);
-      sliderValue.textContent = slider.value;
-  
-      slider.addEventListener('input', (event) => {
-        event.preventDefault();
-        const key = slider.id;
-        const value = slider.value;
-        sendMessage(key, value);
-      });
-  
-      socket.addEventListener('message', (event) => {
-        const receivedMessage = event.data;
-        const [key, value] = receivedMessage.split(": ");
-        if (key === slider.id) {
-          slider.value = value;
+  const debuggingForm = document.getElementById("debugging-form");
+  const keyInput = document.getElementById("key-input");
+  const valueInput = document.getElementById("value-input");
+  const configDropdown = document.getElementById("config");
+  const saveButton = document.getElementById("saveButton");
+  const resetButton = document.getElementById("resetButton");
+  const socket = new WebSocket(`ws://${location.host}/ws`);
+
+  socket.onopen = () => {
+    console.log("WebSocket connection established.");
+  };
+
+  const sendMessage = (key, value) => {
+    const formattedMessage = `${key}: ${value}`;
+    socket.send(formattedMessage);
+  };
+
+  const handleInputEvent = (event) => {
+    event.preventDefault();
+    const element = event.target;
+    const key = element.id;
+    let value;
+
+    if (element.type === "range") {
+      value = element.value;
+    } else if (element.type === "checkbox") {
+      value = element.checked;
+    } else if (element.type === "select-one") {
+      value = element.value;
+    }
+
+    sendMessage(key, value);
+  };
+
+  const handleSocketMessage = (event) => {
+    const receivedMessage = event.data;
+    const [key, receivedValue] = receivedMessage.split(": ");
+    const value = receivedValue.toLowerCase();
+
+    const element = document.getElementById(key);
+    if (element) {
+      if (element.type === "range") {
+        element.value = value;
+        const sliderValue = document.getElementById(`${key}Value`);
+        if (sliderValue) {
           sliderValue.textContent = value;
         }
-      });
-    });
-  
-    checkboxes.forEach(checkbox => {
-      checkbox.addEventListener('change', (event) => {
-        const key = checkbox.id;
-        const value = checkbox.checked;
-        sendMessage(key, value);
-      });
-  
-      socket.addEventListener('message', (event) => {
-        const receivedMessage = event.data;
-        const [key, receivedValue] = receivedMessage.split(": ");
-        const value = receivedValue.toLowerCase();  // Assign the converted lowercase value to a new variable
+      } else if (element.type === "checkbox") {
+        element.checked = value === "true";
+      } else if (element.type === "select-one") {
+        element.value = value;
+      }
+    }
+  };
 
-        const checkbox = document.getElementById(key);
-        if (checkbox) {
-          checkbox.checked = value === 'true';
-        }
-      });
-    });
+  document.addEventListener("input", handleInputEvent);
+  socket.addEventListener("message", handleSocketMessage);
 
-    configDropdown.addEventListener("change", (event) => {
-      const value = event.target.value;
-      const key = event.target.id;
-      sendMessage(key, value);
-    });
-    saveButton.addEventListener("click", () => {
-      const key = "save_config";
-      const value = configDropdown.value;
-      sendMessage(key, value);
-    });
-  
-    resetButton.addEventListener("click", () => {
-      const key = "config";
-      const value = configDropdown.value;
-      sendMessage(key, value);
-    });
+  saveButton.addEventListener("click", () => {
+    const key = "save_config";
+    const value = configDropdown.value;
+    sendMessage(key, value);
   });
-  
+
+  resetButton.addEventListener("click", () => {
+    const key = "config";
+    const value = configDropdown.value;
+    sendMessage(key, value);
+  });
+});
