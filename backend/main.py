5cb47159fe2e356f4e12d190dc56e327117ee969
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2023-07-05 16:21:51 -0400

    Cleaned out unused code

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,13 @@
 import threading
 import uvicorn
-#from network import frontend, network_setup_event, app
 from detection import process_frames
-from capture import capture_frames#, process_event
+from capture import capture_frames
 from web import app
 from constants import SOCKET_IP
-from send import send_frames
+
 
 capture_thread = threading.Thread(target = capture_frames)
 capture_thread.start()
-
-#process_event.wait()
-
-#network_thread = threading.Thread(target=frontend)
-#network_thread.start()
-
-#network_setup_event.wait()
-
-#send_thread = threading.Thread(target=send_frames)
-#send_thread.start()
 
 processing_thread = threading.Thread(target= process_frames)
 processing_thread.start()
