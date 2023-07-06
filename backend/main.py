4bc0a41a005f97cac1ed275c4e844c56a37c35c6
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2023-07-05 23:48:39 -0400

    added send thread

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ import uvicorn
 from detection import process_frames
 from capture import capture_frames
 from web import app
+from send import send_filtered_results
 from constants import SOCKET_IP
 
 
@@ -11,5 +12,8 @@ capture_thread.start()
 
 processing_thread = threading.Thread(target= process_frames)
 processing_thread.start()
+
+results_thread = threading.Thread(target= send_filtered_results)
+results_thread.start()
 
 uvicorn.run(app, host=SOCKET_IP, port=8000)
