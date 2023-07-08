6cbbc9c1883e97d0293cede9a7ecc02c0897d33c
klinefelters <stevenjk03@gmail.com>
2023-07-08 21:12:42 +0200

    Loading the default config on start

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ import threading
 import uvicorn
 from detection import process_frames
 from capture import capture_frames
-from web import app
+from web import app, load_config
 from send import send_filtered_results
 from constants import SOCKET_IP
 
@@ -15,5 +15,7 @@ processing_thread.start()
 
 results_thread = threading.Thread(target= send_filtered_results)
 results_thread.start()
+
+load_config('default')
 
 uvicorn.run(app, host=SOCKET_IP, port=8000)
