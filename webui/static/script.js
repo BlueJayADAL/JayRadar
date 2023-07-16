280f0dbbebffb212a01524402eee5ca9bd683f65
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2023-07-16 18:18:29 -0400

    WebSocket Example implemented

--- webui/static/script.js
+++ webui/static/script.js
@@ -1,2 +1,26 @@
-// Example JavaScript code
-console.log("Hello from script.js!");
+document.addEventListener("DOMContentLoaded", () => {
+  const messagesDiv = document.getElementById("messages");
+  const messageForm = document.getElementById("message-form");
+  const userInput = document.getElementById("user-input");
+  const messageInput = document.getElementById("message-input");
+
+  const socket = new WebSocket(`ws://${location.host}/ws`);
+
+  socket.onopen = () => {
+    console.log("WebSocket connection established.");
+  };
+
+  socket.onmessage = (event) => {
+    const message = event.data;
+    messagesDiv.innerHTML += `<p>${message}</p>`;
+  };
+
+  messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const user = userInput.value;
+    const message = messageInput.value;
+    const formattedMessage = `${user}: ${message}`;
+    socket.send(formattedMessage);
+    messageInput.value = "";
+  });
+});
--- webui/web_ui.py
+++ webui/web_ui.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
@@ -12,8 +12,25 @@ class WebUI:
         self.templates = Jinja2Templates(directory=templates_path)
         self.ip = ip
         self.port = port
+        self.connections = []
 
     def configure_routes(self):
+        @self.app.websocket("/ws")
+        async def websocket_endpoint(websocket: WebSocket):
+            await websocket.accept()
+            # Add the new connection to the list
+            self.connections.append(websocket)
+            while True:
+                try:
+                    data = await websocket.receive_text()
+                    print(f"Server received: {data}")
+                    # Broadcast the received message to all connected clients
+                    for connection in self.connections:
+                        await connection.send_text(data)
+                except WebSocketDisconnect:
+                    self.connections.remove(websocket)
+                    break
+                
         @self.app.get("/")
         async def home(request: Request):
             return self.templates.TemplateResponse("index.html", {"request": request})
