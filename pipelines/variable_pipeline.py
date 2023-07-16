5a296a0833a75e2313d7702e29a26a126d2c0272
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2023-07-16 16:40:31 -0400

    Can dynamically add filters

--- pipelines/variable_pipeline.py
+++ pipelines/variable_pipeline.py
@@ -1,16 +1,49 @@
 from pipelines import Pipeline
+import cv2
 
 class VariablePipeline(Pipeline):
-    def __init__(self, source, output, config, edit_q, *filters):
-        self.source = source
-        self.output = output
-        self.config = config
-        self.edit_q = edit_q
-        self.filters = list(filters)
+    def __init__(self, source, output, filter_q, *filters):
+        super().__init__(source, output, *filters)
+        self.filters = list(self.filters)
+        self.filter_q = filter_q
 
-    def add_pipe(self):
-        pass
+    def check_q(self):
+        while not self.filter_q.empty():
+            command, filter = self.filter_q.get()
+            if command == "add":
+                self.add_filter(filter, index=self.num_filters)
     
-    def pop_pipe(self):
-        pass
+    def add_filter(self, filter, index=0):
+        self.filters.insert(index, filter)
+        self.num_filters += 1
+    
+    def remove_filter(self, filter):
+        try:
+            self.filters.remove(filter)
+        except ValueError:
+            print("Remove_filter function failed in Variable_Pipeline")
+    
+    def del_filter(self, index:int=0):
+        if index > (self.num_filters-1):
+            return False
+        else:
+            del self.filters[index]
+            self.num_filters -= 1
+    
+    def run(self):
+
+        while True:
+            self.check_q()
+            frame, data = self.source.get_frame()
+            if frame is None:
+                break
+
+            for filter in self.filters:
+                frame, data = filter.process_frame(frame, data)
+
+            self.output.send_frame(frame, data)
+            
+            cv2.waitKey(1)
+
+        self.cleanup()
     
