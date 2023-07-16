d6b2eb6b326302938e3932866fbe315a24e1ec6f
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2023-07-16 18:06:21 -0400

    Working FastAPI App class!

--- webui/web_ui.py
+++ webui/web_ui.py
@@ -1,15 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
 
 class WebUI:
-    def __init__(self):
+    def __init__(self, ip="0.0.0.0", port:int=8000):
         self.app = FastAPI()
+        static_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
+        templates_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
+        self.app.mount("/static", StaticFiles(directory=static_path), name="static")
+        self.templates = Jinja2Templates(directory=templates_path)
+        self.ip = ip
+        self.port = port
 
     def configure_routes(self):
         @self.app.get("/")
-        def root():
-            return {"message": "Hello, World!"}
+        async def home(request: Request):
+            return self.templates.TemplateResponse("index.html", {"request": request})
 
     def run(self):
         self.configure_routes()
         import uvicorn
-        uvicorn.run(self.app)
+        uvicorn.run(self.app, host=self.ip, port=self.port)
