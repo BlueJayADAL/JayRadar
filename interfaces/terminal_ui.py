532a28883cfda439b167d71e7008eaf6740e4982
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2023-07-17 17:04:06 -0400

    Trying typecasting

--- interfaces/terminal_ui.py
+++ interfaces/terminal_ui.py
@@ -9,13 +9,22 @@ class TerminalUI:
             if key in self.configs["hsv"]:
                 self.configs["hsv"][key] = value
 
+    def update_configs(self, filter, key, value):
+        if filter in self.configs:
+            if key in self.configs[filter]:
+                current_type= type(self.configs[filter][key])
+                
+                try:
+                    self.configs[filter][key] = current_type(value)
+                except ValueError:
+                    print(f"Typecasting failed: '{value}' cannot be converted to {current_type}.")
+
     def run(self):
         while True:
             keys = input("Enter a command: ")
             if keys == "q":
                 break
-            elif keys == "hsv":
-                key = input("Enter a key: ")
-                value = input("Enter a value: ")
-                value = float(value)
-                self.update_hsv(key, value)
+            filter = input("Enter a filter: ")
+            key = input("Enter a key: ")
+            value = input("Enter a value: ")
+            self.update_configs(filter, key, value)
