bab47576f710d78f5e97ded6a77cbfc9e40bc6b3
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2023-07-16 23:27:31 -0400

    Deeplearning filter is configurable

--- main.py
+++ main.py
@@ -11,23 +11,29 @@ if __name__ == "__main__":
     manager = Manager()
 
     filters_q = Queue()
-    
-    hsv_config = manager.dict()
-    
-    hsv_config["brightness"] = 0
-    hsv_config["contrast"] = 1.0
-    hsv_config["saturation"] = 1.0
 
-    rgb_config = manager.dict()
-    
-    rgb_config["red"] = 15
-    rgb_config["green"] = 27
-    rgb_config["blue"] = -23
+    dl_config = manager.dict({
+        "model": "yolov8n.pt",
+        "tx": 320,
+        "ty": 240,
+        "conf": .25,
+        "iou": .7,
+        "half": False,
+        "ss": False,
+        "ssd": False,
+        "max": 7,
+        "img": 640,
+        "class": None
+        })
+
+    hsv_config = manager.dict({"brightness": 0, "contrast": 1.0, "saturation": 1.0})
+
+    rgb_config = manager.dict({"red": 0, "green": 0, "blue": 0})
 
     source = ThreadedSource(device=0, windows=True)
     hsv_pipe = HSVFilter(hsv_config)
     rgb_pipe = RGBFilter(rgb_config)
-    dl_pipe = DeepLearning()
+    dl_pipe = DeepLearning(dl_config)
     output = NTDisplay(verbose=False)
     pipeline = VariablePipeline(source, output, filters_q, dl_pipe)
 
@@ -41,16 +47,22 @@ if __name__ == "__main__":
             break
         elif keys == "b+":
             hsv_config["brightness"] += 5
+            print(f'Brightness = {hsv_config["brightness"]}')
         elif keys == "b-":
             hsv_config["brightness"] -= 5
+            print(f'Brightness = {hsv_config["brightness"]}')
         elif keys == "c+":
-            hsv_config["contrast"] += .1
+            dl_config["conf"] += .05
+            print(f'Confidence = {dl_config["conf"]}')
         elif keys == "c-":
-            hsv_config["contrast"] -= .1
+            dl_config["conf"] -= .05
+            print(f'Confidence = {dl_config["conf"]}')
         elif keys == "s+":
             hsv_config["saturation"] += .1
+            print(f'Saturation = {hsv_config["saturation"]}')
         elif keys == "s-":
             hsv_config["saturation"] -= .1
+            print(f'Saturation = {hsv_config["saturation"]}')
         elif keys == "r+":
             rgb_config["red"] += 5
         elif keys == "r-":
@@ -63,6 +75,8 @@ if __name__ == "__main__":
             rgb_config["blue"] += 5
         elif keys == "l-":
             rgb_config["blue"] -= 5
+        elif keys == "dl":
+            filters_q.put(["add", 0, dl_pipe])
         elif keys == "rgb":
             filters_q.put(["add", 0, rgb_pipe])
         elif keys == "hsv":
--- pipelines/filters/deep_learning.py
+++ pipelines/filters/deep_learning.py
@@ -2,21 +2,36 @@ from ultralytics import YOLO
 import math
 
 class DeepLearning:
-    def __init__(self, model="yolov8n.pt"):
-        self.model_name = model
+    def __init__(self, config: dict={
+        "model": "yolov8n.pt",
+        "tx": 320,
+        "ty": 240,
+        "conf": .25,
+        "iou": .7,
+        "half": False,
+        "ss": False,
+        "ssd": False,
+        "max": 7,
+        "img": 640,
+        "class": None
+        }):
+
+        self.config = config
 
     def initialize(self):
-        self.model = YOLO(self.model_name)
+        self.model = YOLO(self.config["model"])
 
-    def _filter_results(self, result, tx=320, ty=240):
+    def _filter_results(self, result):
+
         if not result.boxes:
             return None, False
+        
         closest_box = None
         min_distance = math.inf
 
         for box in result.boxes:
             cx, cy, w, h = [round(x, 4) for x in box.xywh[0].tolist()]
-            distance = math.sqrt((cx - tx)**2 + (cy - ty)**2)
+            distance = math.sqrt((cx - self.config["tx"])**2 + (cy - self.config["ty"])**2)
 
             if distance < min_distance:
                 min_distance = distance
@@ -27,7 +42,19 @@ class DeepLearning:
         return closest_box, True
 
     def process_frame(self, frame, data):
-        results = self.model.predict(frame, verbose=False)
+        
+        results = self.model.predict(
+            frame,
+            conf=self.config["conf"],
+            iou=self.config["iou"],
+            half=self.config["half"],
+            save=self.config["ss"],
+            save_txt=self.config["ssd"],
+            max_det=self.config["max"],
+            classes=self.config["class"],
+            imgsz = self.config["img"],
+            verbose=False
+        )
         
         result, success = self._filter_results(results[0])
         
