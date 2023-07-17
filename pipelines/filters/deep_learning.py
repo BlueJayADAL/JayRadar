e7b215624a4c2b2dbbbb4257c76de0e9e6d2c9ea
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2023-07-16 21:28:15 -0400

    Deeplearning now adds filtered data to pipeline

--- pipelines/filters/deep_learning.py
+++ pipelines/filters/deep_learning.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+import math
 
 class DeepLearning:
     def __init__(self, model="yolov8n.pt"):
@@ -7,10 +8,37 @@ class DeepLearning:
     def initialize(self):
         self.model = YOLO(self.model_name)
 
+    def _filter_results(self, result, tx=320, ty=240):
+        if not result.boxes:
+            return None, False
+        closest_box = None
+        min_distance = math.inf
+
+        for box in result.boxes:
+            cx, cy, w, h = [round(x, 4) for x in box.xywh[0].tolist()]
+            distance = math.sqrt((cx - tx)**2 + (cy - ty)**2)
+
+            if distance < min_distance:
+                min_distance = distance
+                id = float(box.cls)
+                prob = float(box.conf)
+                closest_box = [cx, cy, w, h, id, prob]
+
+        return closest_box, True
+
     def process_frame(self, frame, data):
         results = self.model.predict(frame, verbose=False)
+        
+        result, success = self._filter_results(results[0])
+        
         annotated_frame = results[0].plot()
-        data['result'] = results[0]
+        
+        if success:
+            data['tx'], data['ty'], data['tw'], data['th'], data['id'], data['tc'] = result
+            data['te'] = True
+        else:
+            data['te'] = True
+        
         return annotated_frame, data
     
     def release(self):
