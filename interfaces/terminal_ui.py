6e52a52eaa03729c3d940e62e84f4178f60fd5df
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2023-07-17 17:14:21 -0400

    Added exception if current value is none

--- interfaces/terminal_ui.py
+++ interfaces/terminal_ui.py
@@ -11,13 +11,23 @@ class TerminalUI:
 
     def update_configs(self, filter, key, value):
         if filter in self.configs:
+            
             if key in self.configs[filter]:
-                current_type= type(self.configs[filter][key])
+
+                if value is None:
+                    self.configs[filter][key] = None
                 
-                try:
-                    self.configs[filter][key] = current_type(value)
-                except ValueError:
-                    print(f"Typecasting failed: '{value}' cannot be converted to {current_type}.")
+                current_value = self.configs[filter][key]
+                current_type= type(current_value)
+
+                if current_value is None:
+                    self.configs[filter][key] = None
+                    
+                else:
+                    try:
+                        self.configs[filter][key] = current_type(value)
+                    except ValueError:
+                        print(f"Typecasting failed: '{value}' cannot be converted to {current_type}.")
 
     def run(self):
         while True:
