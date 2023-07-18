dacb3c455162a226b49dc4340baae919b1e4d80f
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2023-07-18 00:14:45 -0400

    Running the manager through it's paces. Holds up!

--- manager_testing.py
+++ manager_testing.py
@@ -7,6 +7,29 @@ if __name__ == "__main__":
 
     manager = PipelineManager(source, output)
 
-    input("Hit Enter to stop...")
+
+    while True:
+        command = input("Enter a command: ")
+        if command == "quit":
+            break
+        elif command == "delete":
+            index = input("Enter an index: ")
+            manager.delete_filter(int(index))
+        elif command == "add":
+            filter = input("Enter a filter type: ")
+            if filter == "hsv":
+                index = input("Enter an index: ")
+                manager.add_hsv(int(index))
+            elif filter == "dl":
+                index = input("Enter an index: ")
+                manager.add_dl(int(index))
+            elif filter == "rgb":
+                index = input("Enter an index: ")
+                manager.add_rgb(int(index))
+        elif command == "update":
+            filter = input("Enter a filter type: ")
+            key = input("Enter a key: ")
+            value = input("Enter a value: ")
+            manager.update_configs(filter, key, value)
 
     manager.release()
