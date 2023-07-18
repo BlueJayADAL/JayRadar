fddb41e3d4fcd6da98a8b98944eae135a9cc6f92
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2023-07-18 18:08:28 -0400

    Added cv2 interface

--- interfaces/__init__.py
+++ interfaces/__init__.py
@@ -2,3 +2,4 @@
 
 from .web_ui import WebUI
 from .terminal_ui import TerminalUI
+from .cv2_ui import CV2UI
---
+++ interfaces/cv2_ui.py
@@ -0,0 +1,69 @@
+from pipelines import PipelineManager
+from multiprocessing import Queue
+import cv2
+
+class CV2UI:
+    """
+    CV2UI class for providing a terminal-based user interface to interact with a video processing pipeline.
+
+    This class allows the user to view frames from the video processing pipeline in a window using OpenCV and
+    interact with the pipeline by pressing keys on the keyboard. The available commands are:
+    - Press "q" to quit the application.
+    - Press "d" to delete the first filter in the pipeline.
+    - Press "y" to add a DeepLearning filter at the beginning of the pipeline.
+
+    The user can interact with the pipeline managed by a PipelineManager object using this terminal user interface.
+    """
+
+    def __init__(self, manager: PipelineManager, q_in: Queue):
+        """
+        Initialize the CV2UI object.
+
+        Args:
+            manager (PipelineManager): The PipelineManager object used to manage the video processing pipeline.
+            q_in (Queue): The multiprocessing Queue to receive frames from the video processing pipeline.
+        """
+        self.manager = manager
+        self.q_in = q_in
+
+    def run(self):
+        """
+        Run the terminal user interface.
+
+        The method provides a loop where the user can view frames from the video processing pipeline in a window using
+        OpenCV and interact with the pipeline by pressing keys on the keyboard. The available commands are:
+        - Press "q" to quit the application.
+        - Press "d" to delete the first filter in the pipeline.
+        - Press "y" to add a DeepLearning filter at the beginning of the pipeline.
+
+        The method continuously retrieves frames from the multiprocessing Queue and displays them in the "CV2 UI" window.
+        It waits for keyboard input and executes the corresponding actions based on the pressed keys. The application
+        will continue running until the user presses "q" to quit.
+
+        After exiting the loop, the method releases the resources used by the pipeline manager and terminates the
+        associated pipeline process.
+        """
+        while True:
+            frame = self.q_in.get()
+
+            if frame is not None:
+                cv2.imshow("CV2 UI", frame)
+            
+            key = cv2.waitKey(1)
+
+            if key == ord("q"):
+                break
+            elif key == ord("d"):
+                self.manager.delete_filter(0)
+            elif key == ord("y"):
+                self.manager.add_dl(0)
+
+        self.release()
+
+    def release(self):
+        """
+        Release resources and terminate the pipeline manager.
+
+        The method releases the resources used by the pipeline manager and terminates the associated pipeline process.
+        """
+        self.manager.release()
