3ba7fe2ec010f8af1869ed21d4c2337d9b467a55
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2023-07-18 17:54:28 -0400

    Made TerminalUI work with pipeline manager w/ docs

--- interfaces/terminal_ui.py
+++ interfaces/terminal_ui.py
@@ -1,49 +1,69 @@
-# Trying to make a more proper user interface
+from pipelines import PipelineManager
 
 class TerminalUI:
-    def __init__(self, configs:dict, filter_q):
-        self.configs = configs
-        self.filter_q = filter_q
+    """
+    TerminalUI class for providing a terminal-based user interface to interact with a video processing pipeline.
 
-    def update_hsv(self, key, value):
-        if "hsv" in self.configs:
-            if key in self.configs["hsv"]:
-                self.configs["hsv"][key] = value
+    This class allows the user to input commands to add and delete filters, update filter configurations,
+    and save/load configurations from JSON files. The user can interact with the pipeline managed by a
+    PipelineManager object using this terminal user interface.
+    """
 
-    def update_configs(self, filter, key, value):
-        if filter in self.configs:
-            
-            if key in self.configs[filter]:
+    def __init__(self, manager: PipelineManager):
+        """
+        Initialize the TerminalUI object.
 
-                if value is None:
-                    self.configs[filter][key] = None
-                
-                current_value = self.configs[filter][key]
-                current_type= type(current_value)
-
-                if current_value is None:
-                    self.configs[filter][key] = None
-
-                else:
-                    try:
-                        self.configs[filter][key] = current_type(value)
-                    except ValueError:
-                        print(f"Typecasting failed: '{value}' cannot be converted to {current_type}.")
+        Args:
+            manager (PipelineManager): The PipelineManager object used to manage the video processing pipeline.
+        """
+        self.manager = manager
 
     def run(self):
+        """
+        Run the terminal user interface.
+
+        The method provides a loop where the user can enter commands to interact with the video processing pipeline.
+        The available commands are "quit," "delete," "add," "update," "save," and "load." The user can quit the
+        application with the "quit" command, delete a filter by index with the "delete" command, add a filter with
+        the "add" command, update filter configurations with the "update" command, and save/load configurations
+        from JSON files with the "save" and "load" commands, respectively.
+        """
         while True:
-            keys = input("Enter a command: ")
-            if keys == "q":
+            command = input("Enter a command: ")
+            if command == "quit":
                 break
-            elif keys =="update":
-                filter = input("Enter a filter: ")
+            elif command == "delete":
+                index = input("Enter an index: ")
+                self.manager.delete_filter(int(index))
+            elif command == "add":
+                filter_type = input("Enter a filter type: ")
+                if filter_type == "hsv":
+                    index = input("Enter an index: ")
+                    self.manager.add_hsv(int(index))
+                elif filter_type == "dl":
+                    index = input("Enter an index: ")
+                    self.manager.add_dl(int(index))
+                elif filter_type == "rgb":
+                    index = input("Enter an index: ")
+                    self.manager.add_rgb(int(index))
+            elif command == "update":
+                filter_type = input("Enter a filter type: ")
                 key = input("Enter a key: ")
                 value = input("Enter a value: ")
-                self.update_configs(filter, key, value)
-            elif keys == "delete":
-                index = input("Enter a desired Index: ")
-                try:
-                    index = int(index)
-                    self.filter_q.put(["delete", index, None])
-                except ValueError:
-                    print("Cannot make index into an Int")
+                self.manager.update_configs(filter_type, key, value)
+            elif command == "save":
+                file_name = input("Enter a filename: ")
+                self.manager.save_to_json(file_name)
+            elif command == "load":
+                file_name = input("Enter a filename: ")
+                self.manager.load_from_json(file_name)
+
+        self.release()
+
+    def release(self):
+        """
+        Release resources and terminate the pipeline manager.
+
+        The method releases the resources used by the pipeline manager and terminates the associated pipeline process.
+        """
+        self.manager.release()
