d7d07e9874ceabb080050c623d67393552639f69
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2023-07-28 17:17:48 -0400

    Pipeline now loads default on launch.

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 from multiprocessing import Queue, set_start_method
 from pipelines import PipelineManager
 from pipelines.sources import ThreadedSource
-from pipelines.outputs import NTSend  # , NTDisplay
-from interfaces import WebUI  # , TerminalUI, CV2UI
+from pipelines.outputs import NTSend
+from interfaces import WebUI
 
 
 if __name__ == "__main__":
     set_start_method('spawn')  # Set the start method for multiprocessing
-    source = ThreadedSource(device=0)
+    source = ThreadedSource(device=0, windows=True)
     shared_q = Queue(maxsize=1)
     output = NTSend(shared_q)
 
     manager = PipelineManager(source, output)
-
-    # manager.add_dl(0)
-    # manager.add_hsv(0)
-    # manager.add_rgb(0)
+    manager.load_from_json("./configs/default.json")
 
     ui = WebUI(manager, shared_q)
     ui.run()
