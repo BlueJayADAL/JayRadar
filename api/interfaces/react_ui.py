8a9c552389be1975c5d0d66e6bb04977d562ce6c
Ethan Lajeunesse <ethan@lajfam.com>
2023-08-06 18:18:52 -0400

    Add comments

--- api/interfaces/react_ui.py
+++ api/interfaces/react_ui.py
@@ -18,6 +18,16 @@ class ReactUI:
         nt_ip="10.1.32.27",
         nt_table="JayRadar"
     ):
+        """Initializes a ReactUI instance.
+
+        Args:
+            manager (PipelineManager): An instance of the PipelineManager class.
+            q_in (Queue): A multiprocessing.Queue for input frames.
+            ip (str, optional): The IP address to run the server on. Defaults to "0.0.0.0".
+            port (int, optional): The port to run the server on. Defaults to 8000.
+            nt_ip (str, optional): The IP address of the NetworkTables server. Defaults to "10.1.32.27".
+            nt_table (str, optional): The name of the NetworkTable. Defaults to "JayRadar".
+        """
         self.root = Path(__file__).resolve().parents[2]
 
         self.app = FastAPI()
@@ -32,10 +42,23 @@ class ReactUI:
         self.connections = []
 
     def value_changed(self, table, key, value, isNew):
+        """Callback function for networktables.ValueChangedListener.
+
+        Args:
+            table: The networktables.Table that received the change.
+            key: The key of the entry that changed.
+            value: The new value of the entry.
+            isNew: True if the entry is new, False if it already existed.
+        """
         if key == "pipeline":
             self.manager.load_from_json(f"./configs/{value}.json")
 
     def get_frame(self):
+        """Generator function that yields frames as MJPEG response.
+
+        Yields:
+            bytes: The frame data as MJPEG response.
+        """
         while True:
             frame = self.q_in.get()
 
@@ -49,6 +72,11 @@ class ReactUI:
                        + frame_data + b'\r\n')
 
     async def send_config(self, websocket):
+        """Sends the current configuration to the WebSocket client.
+
+        Args:
+            websocket: The WebSocket connection.
+        """
         active_pipes = self.manager.get_active_pipes()
         config = self.manager.get_configs_copy()
         for pipe in active_pipes:
@@ -57,6 +85,13 @@ class ReactUI:
                 await websocket.send_text(f"{pipe}/{key}: {value}")
 
     async def manage_websocket_info(self, pipe, key, value):
+        """Manages configuration updates received from the WebSocket client.
+
+        Args:
+            pipe: The name of the pipeline.
+            key: The configuration key.
+            value: The new configuration value.
+        """
         if key == "active":
             if value.lower() == "false":
                 self.manager.delete_pipe(pipe)
@@ -73,11 +108,13 @@ class ReactUI:
             self.manager.update_configs(pipe, key, value)
 
     def configure_static(self):
+        """Configures the static files for the FastAPI app."""
         client_build_path = self.root / "client/dist"
         self.app.mount(
             "/static", StaticFiles(directory=client_build_path), name="static")
 
     def configure_routes(self):
+        """Configures the routes for the FastAPI app."""
         # Set up the networktables for the app
         NetworkTables.initialize(server=self.nt_ip)
         self.table = NetworkTables.getTable(self.nt_table)
@@ -116,14 +153,17 @@ class ReactUI:
 
         @self.app.get("/{full_path:path}")
         async def serve_react_app():
+            # Route for serving the React app. Any routes not covered by the API will be served the React app.
             client_build_path = self.root / "client/dist/index.html"
             return FileResponse(client_build_path)
 
     def run(self):
+        """Runs the FastAPI app and starts the server."""
         self.configure_routes()
         import uvicorn
         uvicorn.run(self.app, host=self.ip, port=self.port)
         self.release()
 
     def release(self):
+        """Releases any resources used by the ReactUI instance."""
         self.manager.release()
